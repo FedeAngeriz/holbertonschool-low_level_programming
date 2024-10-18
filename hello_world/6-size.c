19aa5774dc06ab8e8bd0e5ae64bef0667bac944d
Fede Angeriz <10086@holbertonstudents.com>
2024-10-18 17:51:18 +0000

    El 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	printf("Size of a char: %d byte\nª, (int) sizeof(char)");
+	printf("Size of an int: %d byte\n", (int) sizeof(int)");
+	printf("Size of a long int: %d byte\n", (int) sizeof(long int)");
+	printf("Size of a long long int: %d byte\n", (int) sizeof(long long int)");
+	printf("Size of a float: %d byte\n", (int) sizeof(float)");
+
+	return (0);
+}
