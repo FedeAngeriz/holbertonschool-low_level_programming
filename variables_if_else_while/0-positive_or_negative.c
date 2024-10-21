9feb9c91fc4aa155caa6b36427e2e13c35d9b751
FedeAngeriz <10086@holbertonstudents.com>
2024-10-21 14:54:12 +0000

    El cero del 2

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** betty style doc for function main goes there
+ * main - positivo-negativo
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d" , n);
+	if (n > 0)
+	{
+		printf("is positive \n");
+	}
+	else if (n < 0)
+	{
+		printf("is negative \n");
+	}
+	else
+	{
+		printf("is zero \n");
+	}
+	/* your code goes there */
+	return (0);
+}
