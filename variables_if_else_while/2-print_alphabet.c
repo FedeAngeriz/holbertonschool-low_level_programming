c1dfdbda35f16fbbdc08b240a27e7bf3149615ed
FedeAngeriz <10086@holbertonstudents.com>
2024-10-21 18:59:41 +0000

    El 2 del 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char brahian;
+
+	for (brahian = 'a'; brahian <= 'z'; brahian++)
+	{
+	putchar(brahian);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
