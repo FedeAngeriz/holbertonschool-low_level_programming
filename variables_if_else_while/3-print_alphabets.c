bb0b49ad47504b60d831061fa93268ade4b55528
FedeAngeriz <10086@holbertonstudents.com>
2024-10-21 20:08:30 +0000

    El 3 del 2

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-	putchar(low);
-	}
-	{
-	putchar('\n');
+	putchar(low)
 	}
 	for (up = 'A'; up <= 'Z'; up++)
 	{
 	putchar(up);
+	}
+	{
+        putchar('\n');
 	}
 	return (0);
 }
