5a2b4d6c3dff0501f15dbd0002d6c2072a536d91
FedeAngeriz <10086@holbertonstudents.com>
2024-10-21 15:15:37 +0000

    El cero del 2

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	printf("%d", n);
 	if (n > 0)
 	{
-		printf("is positive \n");
+		printf("is positive\n");
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		printf("is negative \n");
+		printf("is zero\n");
 	}
 	else
 	{
-		printf("is zero \n");
+		printf("is negative\n");
 	}
 	/* your code goes there */
 	return (0);
