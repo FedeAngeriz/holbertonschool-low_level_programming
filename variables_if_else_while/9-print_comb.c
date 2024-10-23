f2d0e24b00ba0c5005fe7053eb680ff399eed245
FedeAngeriz <10086@holbertonstudents.com>
2024-10-23 14:57:51 +0000

    EL 9 del 2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,11 +10,13 @@ int main(void)
 	int var;
 
 	for (var = 0; var < 10; var++)
+	{
 	putchar(var + '0');
-	if (var < 10)
+	if (var < 9)
 	{
 		putchar(',');
 		putchar(' ');
+	}
 	}
 	putchar('\n');
 
