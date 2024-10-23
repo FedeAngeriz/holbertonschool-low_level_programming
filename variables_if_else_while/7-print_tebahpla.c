2bb8f3810551299bf85b7758b313fee6f0aaea6e
FedeAngeriz <10086@holbertonstudents.com>
2024-10-23 14:12:22 +0000

    El 7 del 2

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char braian;
+
+	for (braian = 'z'; braian >= 'a'; braian--)
+	{
+	putchar(braian);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
