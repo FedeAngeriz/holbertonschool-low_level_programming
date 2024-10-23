136d8885f717472c83c55da1e2f94fe13dac513d
FedeAngeriz <10086@holbertonstudents.com>
2024-10-23 20:23:04 +0000

    Subiendo lo ultimo

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - 1-alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+	_putchar(alp);
+	}
+	putchar('\n');
+
+}
