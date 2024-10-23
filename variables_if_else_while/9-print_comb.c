88ed437bd87d9b3d4659e8380f0a89363f0deef1
FedeAngeriz <10086@holbertonstudents.com>
2024-10-23 14:52:50 +0000

    El 9 del 2

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - 9-variable
+ * Return: 0
+ */
+
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 10; var++)
+	putchar(var + '0');
+	if (var < 10)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
