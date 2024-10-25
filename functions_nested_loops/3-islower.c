d2ba920b75229baacdeb7553c7d59a6aa8d82fbe
FedeAngeriz <10086@holbertonstudents.com>
2024-10-25 13:11:18 +0000

    El 3 del 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_alphabet - 1-alphabet
+ *
+ * Return: void
+ */
+
+int _islower(int c)
+{
+
+
+	if (islower(c))
+	return (1);
+	else 
+		return(0);
+
+
+}
