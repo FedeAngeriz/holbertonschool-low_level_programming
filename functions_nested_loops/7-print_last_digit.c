eb3df17b7558fb93a3a9c9cbb5388342752a3507
FedeAngeriz <10086@holbertonstudents.com>
2024-10-26 04:42:52 +0000

    El 7 del 3

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - imprime el ultimo numero
+ * @num: variable
+ *Return: el ultimo digito
+ */
+
+int print_last_digit(int num)
+{
+	int res;
+	
+	num % 10 = res;
+	return (res);
+}
