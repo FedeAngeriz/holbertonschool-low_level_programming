9d28255db23d6af0c6699837786d1ce8cbe11aec
FedeAngeriz <10086@holbertonstudents.com>
2024-10-29 02:26:40 +0000

    El 2 del 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplicacion
+ * @a: primero numero
+ * @b: segundo numero
+ * Return: resultado de cuenta
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
