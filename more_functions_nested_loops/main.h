708eedbf78cacaa336be6433de22e3d80a22a3f7
FedeAngeriz <10086@holbertonstudents.com>
2024-10-29 00:04:44 +0000

    Lo nuevo

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void times_table(void);
+void print_to_98(int n);
