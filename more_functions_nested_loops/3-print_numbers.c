9cd74a96a1e10589bf4654a29a0a33d438b8c484
FedeAngeriz <10086@holbertonstudents.com>
2024-10-29 03:06:24 +0000

    El 3 del 1

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - conteo de numeros
+ * @n: numero va sumando
+ * Return:
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	_putchar (n);
+	_putchar ('\n');
+}
