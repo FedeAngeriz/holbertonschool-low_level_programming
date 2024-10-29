68413d2ba91bd16c125e6399c409742acdc8e200
FedeAngeriz <10086@holbertonstudents.com>
2024-10-29 01:00:05 +0000

    El 0 y el 1 van bien

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,20 @@
 #include "main.h"
 
 /**
-*
+* _isupper - que tipo de letra es
+* @c: que letra entra
+* Return: 1 si la letra es Mayus, si no es 0
 */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
