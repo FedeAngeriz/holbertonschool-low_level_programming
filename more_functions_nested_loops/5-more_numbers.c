723ee3d71e7e9d4f9b057433864da5857e19d9ed
FedeAngeriz <10086@holbertonstudents.com>
2024-10-29 18:50:11 +0000

    El 5 del 1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers
+ *
+ * Return:
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int line;
+
+	for (line = 1; line <= 10; line++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar (n / 10 + '0');
+			}
+			_putchar (n % 10 + '0');
+		}
+		_putchar ('\n');
+	}
+}
