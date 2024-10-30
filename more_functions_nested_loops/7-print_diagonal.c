437803b7131f54ab5bfe045183325a9994027278
FedeAngeriz <10086@holbertonstudents.com>
2024-10-30 14:10:28 +0000

    El 7 del 1

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,16 +3,28 @@
 /**
  * print_diagonal - imprime diagonal
  * @n: variable
+ * @e: variable espacio blanco
  *Return: salta de linea
  */
 
 void print_diagonal(int n)
 {
-	int l;
-
-	for (l = 0; l < n; l++)
+	int l, e;
+	
+	if (n <= 0)
 	{
-		_putchar ('\\');
+		_putchar ('\n');
 	}
-	_putchar ('\n');
+	else
+	{
+		for (e = 1; e <= n; e++)
+		{
+			for (l = 0; l < e; l++)
+			{	
+				_putchar (' ');
+			}
+				_putchar ('\\');
+				_putchar ('\n');			
+		}
+	}
 }
