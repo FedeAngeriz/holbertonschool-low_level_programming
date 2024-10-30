1cbd6c7a7b6fa009d6ad720d2cfbec7aeecc6460
FedeAngeriz <10086@holbertonstudents.com>
2024-10-30 16:26:48 +0000

    El 8 del 1

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,26 +2,28 @@
 
 /**
  * print_square - imprime
- * @n: variable
+ * @size: variable
  *
  */
 
 void print_square(int size)
 {
 	int l, e;
-	
+
 	if (size <= 0)
 	{
 		_putchar ('\n');
 	}
+	else
+	{
 
 	for (l = 0; l < size; l++)
 	{
-		for (e = 0; e < l; e++)
+		for (e = 1; e <= size; e++)
 		{
 			_putchar ('#');
 		}
 	_putchar ('\n');
 	}
-
+	}
 }
