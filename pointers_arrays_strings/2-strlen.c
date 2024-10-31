d1b16cd21bc0a0b8820762b0c7278ee5b8a219f4
FedeAngeriz <10086@holbertonstudents.com>
2024-10-31 18:50:22 +0000

    El 2 el 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - largo de cadena
+ * @s: puntero
+ * Return: el largo
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	return (l);
+}
