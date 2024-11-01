0a3f6a7a0a6f11aece9aba64e63bf5d78576bf21
FedeAngeriz <10086@holbertonstudents.com>
2024-11-01 15:11:35 +0000

    El 3 del 2

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - 
- * @str:
- *Return: 
+ * _puts - cuenta el largo
+ * @str: string
+ *Return: el largo
  */
 
 void _puts(char *str)
 {
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		_putchar (str[i]);
+		i++;
+	}
+	_putchar ('\n');
 }
