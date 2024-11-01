5255e9e7fd893cb3de194883ec310485bdb95c99
FedeAngeriz <10086@holbertonstudents.com>
2024-11-01 16:09:35 +0000

    El 4 del 2

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime largo
+ * @s: string
+ *Return: el largo al reves
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	i--;
+
+	while (i >= '\0')
+	{
+		_putchar (s[i]);
+		i--;
+	}
+	_putchar ('\n');
+}
