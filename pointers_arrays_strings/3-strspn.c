7fdb64829b5a43315756ace731358b7255ff2b44
FedeAngeriz <10086@holbertonstudents.com>
2024-11-07 16:26:37 +0000

    El 3 del 1

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,34 @@
 #include "main.h"
 
 /**
- * _strspn - 
- * @s: puntero
- * @accept: puntero
- * Return:
+ * _strspn - recorre puntero
+ * @s: puntero con string completo
+ * @accept: puntero con caracter permitido
+ * Return: todos los repetidos hasta el primero que no
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int guardo = 0;
+	char *p;
 
+	while (*s)
+	{
+		p = accept;
+		while (*p)
+		{
+			if (*s == *p)
+			{
+				guardo++;
+				break;
+			}
+		p++;
+		}
+	if (*p == '\0')
+	{
+		break;
+	}
+	s++;
+	}
+	return (guardo);
 }
