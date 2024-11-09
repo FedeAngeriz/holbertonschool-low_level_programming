b5eb426de7476255abbc18aa6ad8c27afbc57799
FedeAngeriz <10086@holbertonstudents.com>
2024-11-09 21:54:12 +0000

    Lo nuevo

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * 
+ * @
+ * Return:
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int num1 = 0;
+	int num2 = 0;
+	int recorre = 0;
+	int *p1 = a;
+	int *p2 = a (size - 1);
+
+	for (; recorre < size; recorre++)
+	{
+
+	}
+}
