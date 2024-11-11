71d6b7815797a2c1fa862a1ea0b6614dcbd79c96
FedeAngeriz <10086@holbertonstudents.com>
2024-11-11 03:02:28 +0000

    El 8 del 1

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 
 /**
- * 
- * @
- * Return:
+ *print_diagsums- funcion que suma matrices en digonal
+ *@a:puntero con las pociciones
+ *@size: puntero con el tamaÃo
+ *
  */
+
+
 
 void print_diagsums(int *a, int size)
 {
-	int num1 = 0;
-	int num2 = 0;
-	int recorre = 0;
-	int *p1 = a;
-	int *p2 = a (size - 1);
+	int sum1 = 0, sum2 = 0;
+	int *puntero1 = a;
+	int *puntero2 = a + (size - 1);
+	int i = 0;
 
-	for (; recorre < size; recorre++)
+	for (i = 0; i < size; i++)
 	{
-
+		sum1 += *puntero1;
+		puntero1 += size + 1;
 	}
+
+
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += *puntero2;
+		puntero2 += size - 1;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
