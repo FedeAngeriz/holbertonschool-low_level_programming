eb77de23075c41e129bf74f5647ae52a45c7f1c3
FedeAngeriz <10086@holbertonstudents.com>
2024-11-14 17:19:54 +0000

    El 3 del 1

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,12 +1,48 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * 
- * @
- *Return: 
+ * alloc_grid - grilla de filas y columnas
+ * @width: variable fila
+ * @height: variable columnaa
+ *Return: grilla de filas y columnas con datos a cero
  */
 
 int **alloc_grid(int width, int height)
 {
-	
+	int **asignar;
+	int columna, fila;
+
+	if (width < 0 || height < 0)
+	{
+		return (NULL);
+	}
+
+	asignar = malloc(height * sizeof(int **));
+
+	if (asignar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (columna = 0; columna < height; columna++)
+	{
+		asignar[columna] = malloc(width * sizeof(int));
+
+		if (asignar == NULL)
+		{
+			for (fila = 0; fila < width; fila++)
+			{
+				free(asignar[fila]);
+			}
+			free(asignar[columna]);
+			return (NULL);
+		}
+
+		for (fila = 0; fila < width; fila++)
+		{
+			asignar[columna][fila] = 0;
+		}
+	}
+	return (asignar);
 }
