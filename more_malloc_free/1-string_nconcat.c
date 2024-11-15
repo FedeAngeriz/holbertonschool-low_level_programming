0069e4f90f4607ae404da052b0841a2ad8c50821
FedeAngeriz <10086@holbertonstudents.com>
2024-11-15 16:15:33 +0000

    El 1 del 2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,25 +9,27 @@
 
 int _strlen(char *s)
 {
-	int l;
+	int num = 0, l;
 
 	for (l = 0; s[l] != '\0'; l++)
 	{
+		num++;
 	}
-	return (l);
+	return (num);
 }
 
 /**
- * str_concat - concatenar 2 strings
+ * string_nconcat - concatenar 2 strings
  * @s1: puntero
  * @s2: puntero
+ * @n: variable comparativa con s2
  *Return: una cadena nueva con las anteriores sumadas
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *copia;
-	int num, num2;
+	int num, num2, fin;
 
 	if (s1 == NULL)
 	{
@@ -38,34 +40,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	if (n >= (_strlen(s2)))
+	if (n < (_strlen(s2)))
 	{
+		fin = n;
 		copia = malloc(_strlen(s1) + _strlen(s2) + 1);
 	}
 	else
 	{
+		fin = _strlen(s2);
 		copia = malloc(n + _strlen(s1) + 1);
 	}
 	
 	if (copia == NULL)
-	{
-		return (NULL);
-	}
-
-	if (copia == NULL)
 		return (NULL);
 
-	for (num = 0; s1[num] != '\0'; num++)
-	{
+	for (num = 0; num < _strlen(s1); num++)
 		copia[num] = s1[num];
-	}
-	
-	if (n >= (_strlen(s2)))
+
+	if (n < (_strlen(s2)))
 	{
-		for (num2 = 0; s2[num2] != '\0'; num2++)
-		{
+		for (num2 = 0; num2 < fin; num2++)
 			copia[num + num2] = s2[num2];
-		}
 	}
 	else
 	{
