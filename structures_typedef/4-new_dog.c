5488ad518341da8c6d03e031a0b5d80f717dc1c8
FedeAngeriz <10086@holbertonstudents.com>
2024-11-15 23:51:18 +0000

    El 4 del 3

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,65 @@
+#include "dog.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * dog: nuevo perro
+ */
+
+typedef struct new_dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+/**
+ * new_dog - imprimir nueva estructura de dog
+ * @name: variable nombre
+ * @age: variable edad
+ * @owner: variable duenio
+ * Return: si es NULL, nil, si no, el parametro dado
+ */
+
+struct dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_perro;
+
+	new_perro = malloc(sizeof(dog_t));
+
+	if (new_perro == NULL)
+		return (NULL);
+
+	if (name == NULL)
+		name = "";
+
+	new_perro->name = malloc(_strlen(name) + 1);
+
+	if (new_perro->name == NULL)
+	{
+		free(new_perro);
+		return (NULL);
+	}
+
+	strcpy(new_perro->name, name);
+
+	if (owner == NULL)
+		owner = "";
+
+	new_perro->owner = malloc(_strlen(owner) + 1);
+
+	if (new_perro->owner == NULL)
+	{
+		free(new_perro->name);
+		free(new_perro);
+		return (NULL);
+	}
+
+	strcpy(new_perro->owner, owner);
+
+	new_perro->age = age;
+
+	return (new_perro);
+}
