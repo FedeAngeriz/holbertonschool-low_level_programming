484f935bf7f41fe7294bb0f537f94f4da2fa2dd2
FedeAngeriz <10086@holbertonstudents.com>
2024-11-22 20:16:15 +0000

    El 2 del 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		cadena = va_arg(args, char *);
 
 		if (cadena == NULL)
-			printf("(nill)");
+			printf("(nil)");
 		else
 			printf("%s", cadena);
 
