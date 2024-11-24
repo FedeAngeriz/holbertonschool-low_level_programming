138f782b816b30b93731b0c898bec47f0b9b72f6
FedeAngeriz <10086@holbertonstudents.com>
2024-11-24 20:15:55 +0000

    El 3 del 2

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,11 +1,17 @@
 #include "variadic_functions.h"
 
+/**
+ * print_all - imprime cualquier tipo de caracter
+ * @format: variable que recibe cualquier tipo de caracter
+ * Return: imprime cualquier caracter separador por ", ".
+ */
+
 void print_all(const char * const format, ...)
 {
 	char *cadena;
 	unsigned int i = 0;
 	va_list args;
-	
+
 	va_start(args, format);
 
 	while (format && format[i])
@@ -14,24 +20,32 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(args, int));
+				break;
 			case 'i':
 				printf("%i", va_arg(args, int));
+				break;
 			case 'f':
 				printf("%f", va_arg(args, double));
+				break;
 			case 's':
 				cadena = va_arg(args, char *);
-
-			if (cadena == NULL)
-				printf("(nil)");
-			else
-				printf("%s", cadena);
+				if (cadena == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				else
+				{
+					printf("%s", cadena);
+					break;
+				}
 		}
 
-		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+		if (format[(i + 1)] != '\0' && (format[i] == 'c' || format[i] == 'i'
+					|| format[i] == 'f' || format[i] == 's'))
 			printf(", ");
 	i++;
 	}
 	va_end(args);
-
 	printf("\n");
 }
