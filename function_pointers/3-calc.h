b499f64bc9991f26252b5fdcb04ffb9b3eba756d
FedeAngeriz <10086@holbertonstudents.com>
2024-11-24 21:00:26 +0000

    Lo ultimo

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,3 +1,8 @@
+#ifndef _3_CALC_H
+#define _3_CALC_H
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct op - Struct op
  *
@@ -16,4 +21,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
+#endif
