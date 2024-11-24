8b7ffa3808b79c75f779013f43700fe0a5cdba83
FedeAngeriz <10086@holbertonstudents.com>
2024-11-24 20:17:39 +0000

    El 3 del 2

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -34,11 +34,8 @@ void print_all(const char * const format, ...)
 					printf("(nil)");
 					break;
 				}
-				else
-				{
 					printf("%s", cadena);
 					break;
-				}
 		}
 
 		if (format[(i + 1)] != '\0' && (format[i] == 'c' || format[i] == 'i'
