39bfec1de2efce68fb2c6c2f02d401b18075151d
FedeAngeriz <10086@holbertonstudents.com>
2024-12-04 18:27:46 +0000

    Update 0 is 1

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - Lista enlazadas
+ * @h: puntero a siguiente nodo
+ * Return: Todos los elementos de la lista
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodo1 = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("%s\n", h->str);
+		nodo1++;
+		h = h->next;
+	}
+	return (nodo1);
+}
