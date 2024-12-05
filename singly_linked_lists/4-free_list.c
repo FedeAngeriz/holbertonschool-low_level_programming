bb5fb129f002afde0bc2907fa7a30dc181965c0d
FedeAngeriz <10086@holbertonstudents.com>
2024-12-05 20:13:08 +0000

    Update 4 is 1

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - libera memoria
+ * @head: puntero que apunta al primer nodo
+ * Return: libera la memoria de todos los nodos asignados
+ */
+
+void free_list(list_t *head)
+{
+	list_t *recorre;
+
+	while (head != NULL)
+	{
+		recorre = head;
+		head = head->next;
+	}
+	free(recorre->str);
+	free(recorre);
+}
