4d1a314e864bed4b21a8200fea309cd401e14de5
FedeAngeriz <10086@holbertonstudents.com>
2024-12-09 02:05:34 +0000

    Update 8 is 2

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,12 @@
 #include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - elimina nodo en posicion especifica
+ * @head: puntero que apunta a primer nodo
+ * @index: variable, indice de nodo que se elimina
+ * Return: nodo eliminado
+ */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *recorre = *head;
@@ -21,6 +29,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (recorre == NULL)
 		return (-1);
 	if (recorre->prev != NULL)
+		recorre->prev->next = recorre->next;
+	if (recorre->next != NULL)
 		recorre->next->prev = recorre->prev;
 	free(recorre);
 	return (1);
